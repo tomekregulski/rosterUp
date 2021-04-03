e73fa25bb1b42958727ba934d4ec23a1fdfd197e
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-03 11:47:32 -0400

    Add html shell with placeholder classes. Adjust naming in questions.js

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
 const fs = require("fs");
 const questions = require("./questions.js");
 const inquirer = require("inquirer");
+
+const generateHTML = (questions) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${questions.companyName} - ${questions.teamProjectName} Roster</title>
+</head>
+<body>
+    <header>
+        <h1>${questions.companyName}</h1>
+    </header>
+    <main>
+        <section id="overview">
+            <div>
+                <p>${questions.companyDesc}</p>
+                <p>${teamOverview}</p>
+            </div>
+        </section>
+        <section id="team">
+            <div id="manager">
+                <p>${questions.mgrName}</p>
+                <p>${questions.mgrID}</p>
+                <p>${questions.mgrEmail}</p>
+                <p>${questions.mgrPhone}</p>
+            </div>
+            <div id="engineer">
+                <p>${questions.engName}</p>
+                <p>${questions.engID}</p>
+                <p>${questions.engEmail}</p>
+                <p>${questions.engGit}</p>
+            </div>
+            <div id="intern">
+                <p>${questions.intName}</p>
+                <p>${questions.intID}</p>
+                <p>${questions.intEmail}</p>
+                <p>${questions.intSchool}</p>
+            </div>
+    </main>
+
+</body>
+</html>`;
+inquirer.prompt(questions).then((data) => {
+  const filename = `${teamProjectName
+    .toLowerCase()
+    .split(" ")
+    .join("")}-roster.html`;
+  fs.writeFile(filename, generateHTML(data), (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+});
--- questions.js
+++ questions.js
@@ -1,20 +1,25 @@
 const html = [
-    {
-      type: "input",
-      name: "comapnyName",
-      message: "What is the comapny name?",
-    },
-    {
-      type: "input",
-      name: "companyDesc",
-      message: "What is the company description?",
-    },
-    {
-      type: "input",
-      name: "teamOverview",
-      message: "What is the team overview?",
-    },
-  ];
+  {
+    type: "input",
+    name: "companyName",
+    message: "What is the comapny name?",
+  },
+  {
+    type: "input",
+    name: "companyDesc",
+    message: "What is the company description?",
+  },
+  {
+    type: "input",
+    name: "teamProjectName",
+    message: "What is the team's project name?",
+  },
+  {
+    type: "input",
+    name: "teamOverview",
+    message: "What is the team overview?",
+  },
+];
 
 const manager = [
   {
@@ -40,52 +45,52 @@ const manager = [
 ];
 
 const engineer = [
-    {
-      type: "input",
-      name: "engName",
-      message: "What is the engineer's name",
-    },
-    {
-      type: "input",
-      name: "engID",
-      message: "What is the engineer's company ID?",
-    },
-    {
-      type: "input",
-      name: "engEmail",
-      message: "What is the engineer's email address?",
-    },
-    {
-      type: "input",
-      name: "engGit",
-      message: "What is the engineer's gitHub url?",
-    },
-  ];
+  {
+    type: "input",
+    name: "engName",
+    message: "What is the engineer's name",
+  },
+  {
+    type: "input",
+    name: "engID",
+    message: "What is the engineer's company ID?",
+  },
+  {
+    type: "input",
+    name: "engEmail",
+    message: "What is the engineer's email address?",
+  },
+  {
+    type: "input",
+    name: "engGit",
+    message: "What is the engineer's gitHub url?",
+  },
+];
 
-  const intern = [
-    {
-      type: "input",
-      name: intgName",
-      message: "What is the intern's name",
-    },
-    {
-      type: "input",
-      name: "intID",
-      message: "What is the intern's company ID?",
-    },
-    {
-      type: "input",
-      name: "intEmail",
-      message: "What is the intern's email address?",
-    },
-    {
-      type: "input",
-      name: "intSchool",
-      message: "What is the intern's school?",
-    },
-  ];
+const intern = [
+  {
+    type: "input",
+    name: "intName",
+    message: "What is the intern's name",
+  },
+  {
+    type: "input",
+    name: "intID",
+    message: "What is the intern's company ID?",
+  },
+  {
+    type: "input",
+    name: "intEmail",
+    message: "What is the intern's email address?",
+  },
+  {
+    type: "input",
+    name: "intSchool",
+    message: "What is the intern's school?",
+  },
+];
 
 module.exports.teamMember = teamMember;
-module.exports.mgr = mgr;
+module.exports.manager = manager;
 module.exports.engineer = engineer;
 module.exports.intern = intern;
