b366e45ed1eb1d908694cae95676816e6b4f32ec
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-04 12:12:45 -0400

    linked stylesheet

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ const generateHTML = (questions) =>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${questions.companyName} - ${questions.teamProjectName}Roster</title>
+    <link rel="stylesheet" href="style.css" />
+    <title>${questions.companyName} - ${questions.teamProjectName}</title>
 </head>
 <body>
     <header>
-        <h1>$questions.companyName}</h1>
+        <h1>${questions.companyName}</h1>
     </header>
     <main>
         <section id="overview">
-            <div>
-                <p>${questions.companyDesc}</p>
-                <p>${questions.teamOverview}</p>
+            <div id="company">
+              <h2>Our Mission</h2>
+              <p>${questions.companyDesc}</p>
+            </div>
+            <div id="team">
+              <h2>Our Team</h2>
+              <p>${questions.teamOverview}</p>
             </div>
         </section>
     </main>
