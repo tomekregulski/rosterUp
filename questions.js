6562f14408f9177ec895d6fd2754baa0511ba802
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-04 12:46:31 -0400

    Complete template HTML and team prompts. Add basic styling for all sections.

--- questions.js
+++ questions.js
@@ -39,4 +39,44 @@ module.exports = [
     name: "mgrPhone",
     message: "What is the manager's company phone number?",
   },
+  {
+    type: "input",
+    name: "engName",
+    message: "What is the engineer's name",
+  },
+  {
+    type: "input",
+    name: "engID",
+    message: "What is the engineer's company ID?",
+  },
+  {
+    type: "input",
+    name: "engEmail",
+    message: "What is the engineer's email address?",
+  },
+  {
+    type: "input",
+    name: "engGit",
+    message: "What is the engineer's gitHub url?",
+  },
+  {
+    type: "input",
+    name: "intName",
+    message: "What is the intern's name",
+  },
+  {
+    type: "input",
+    name: "intID",
+    message: "What is the intern's company ID?",
+  },
+  {
+    type: "input",
+    name: "intEmail",
+    message: "What is the intern's email address?",
+  },
+  {
+    type: "input",
+    name: "intSchool",
+    message: "What is the intern's school?",
+  },
 ];
