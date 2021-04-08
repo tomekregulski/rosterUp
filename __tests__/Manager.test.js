526f90004534eb3548ecda2e3366db038fa943ee
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-08 13:34:06 -0400

    Add path dependency. Add teamArray functionality.

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,105 +1,105 @@
-// require Employee module - which we will be testing in this suite
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+// // require Employee module - which we will be testing in this suite
+// const Manager = require("../lib/Manager");
+// const Employee = require("../lib/Employee");
 
-// describe Manager
-describe("Manager", () => {
-  // describe Initialization
-  describe("Initialization", () => {
-    // test that it returns an object that is an instance of the Manager class when called with the new keyword
-    // call the constructor
-    // check that it is an instance of Manager
-    it("returns an object that is an instance of the Engineer class when called with the new keyword", () => {
-      const manager = new Manager();
-      expect(manager instanceof Manager).toBe(true);
-    });
+// // describe Manager
+// describe("Manager", () => {
+//   // describe Initialization
+//   describe("Initialization", () => {
+//     // test that it returns an object that is an instance of the Manager class when called with the new keyword
+//     // call the constructor
+//     // check that it is an instance of Manager
+//     it("returns an object that is an instance of the Engineer class when called with the new keyword", () => {
+//       const manager = new Manager();
+//       expect(manager instanceof Manager).toBe(true);
+//     });
 
-    // test that it sets name property based on constructor argument
-    // define a name for the test
-    // call constructor with the test name
-    // check that the resulting name property is equal to the test name
-    it("it sets the name property based on constructor argument", () => {
-      const name = "Giselle";
-      const manager = new Manager(name);
+//     // test that it sets name property based on constructor argument
+//     // define a name for the test
+//     // call constructor with the test name
+//     // check that the resulting name property is equal to the test name
+//     it("it sets the name property based on constructor argument", () => {
+//       const name = "Giselle";
+//       const manager = new Manager(name);
 
-      expect(manager.name).toBe(name);
-    });
+//       expect(manager.name).toBe(name);
+//     });
 
-    // test that it sets id property based on constructor argument
-    // define an id for the test
-    // call constructor with an empty string for the name arg and the test id
-    // check that the resulting id property is equal to the test id
-    it("it sets the id property based on constructor argument", () => {
-      const idNum = 1;
-      const manager = new Manager("", idNum);
+//     // test that it sets id property based on constructor argument
+//     // define an id for the test
+//     // call constructor with an empty string for the name arg and the test id
+//     // check that the resulting id property is equal to the test id
+//     it("it sets the id property based on constructor argument", () => {
+//       const idNum = 1;
+//       const manager = new Manager("", idNum);
 
-      expect(manager.idNum).toBe(idNum);
-    });
+//       expect(manager.idNum).toBe(idNum);
+//     });
 
-    // test that it sets email property based on constructor argument
-    // define an email for the test
-    // call constructor with an empty string for the name arg, any number for the id arg, and the test email
-    // check that the resulting email property is equal to the test email
-    it("it sets the email property based on constructor argument", () => {
-      const email = "test@email.com";
-      const manager = new Manager("", 0, email);
+//     // test that it sets email property based on constructor argument
+//     // define an email for the test
+//     // call constructor with an empty string for the name arg, any number for the id arg, and the test email
+//     // check that the resulting email property is equal to the test email
+//     it("it sets the email property based on constructor argument", () => {
+//       const email = "test@email.com";
+//       const manager = new Manager("", 0, email);
 
-      expect(manager.email).toBe(email);
-    });
-  });
+//       expect(manager.email).toBe(email);
+//     });
+//   });
 
-  // describe getName
-  describe("getName", () => {
-    // test that it returns the name property when the getName() method is called
-    // define a name for the test
-    // call constructor with the test name
-    // call the getName() method and check that the result equals the test name
-    it("returns the name property when the getName() method is called", () => {
-      const name = "Obi";
-      const manager = new Manager(name);
+//   // describe getName
+//   describe("getName", () => {
+//     // test that it returns the name property when the getName() method is called
+//     // define a name for the test
+//     // call constructor with the test name
+//     // call the getName() method and check that the result equals the test name
+//     it("returns the name property when the getName() method is called", () => {
+//       const name = "Obi";
+//       const manager = new Manager(name);
 
-      expect(manager.getName()).toBe(name);
-    });
-  });
+//       expect(manager.getName()).toBe(name);
+//     });
+//   });
 
-  // describe getId
-  describe("getId", () => {
-    // test that it returns the id property when the getId() method is called
-    // define an id for the test
-    // call constructor with an empty string for the name arg and the test id
-    // call the getId() method and check that the result equals the test id
-    it("returns the id property when the getId() method is called", () => {
-      const idNum = 1;
-      const manager = new Manager("", idNum);
+//   // describe getId
+//   describe("getId", () => {
+//     // test that it returns the id property when the getId() method is called
+//     // define an id for the test
+//     // call constructor with an empty string for the name arg and the test id
+//     // call the getId() method and check that the result equals the test id
+//     it("returns the id property when the getId() method is called", () => {
+//       const idNum = 1;
+//       const manager = new Manager("", idNum);
 
-      expect(manager.getId()).toBe(idNum);
-    });
-  });
+//       expect(manager.getId()).toBe(idNum);
+//     });
+//   });
 
-  // desribe getEmail
-  describe("getEmail", () => {
-    // test that it returns the email property when the getEmail() method is called
-    // define an email for the test
-    // call constructor with an empty string for the name arg, any number for the id arg, and the test email
-    // call the getEmail() method and check that the result equals the test email
-    it("returns the email property when the getEmail() method is called", () => {
-      const email = "test@email.com";
-      const manager = new Manager("", 0, email);
+//   // desribe getEmail
+//   describe("getEmail", () => {
+//     // test that it returns the email property when the getEmail() method is called
+//     // define an email for the test
+//     // call constructor with an empty string for the name arg, any number for the id arg, and the test email
+//     // call the getEmail() method and check that the result equals the test email
+//     it("returns the email property when the getEmail() method is called", () => {
+//       const email = "test@email.com";
+//       const manager = new Manager("", 0, email);
 
-      expect(manager.getEmail()).toBe(email);
-    });
-  });
+//       expect(manager.getEmail()).toBe(email);
+//     });
+//   });
 
-  // decsribe getRole
-  describe("getRole", () => {
-    // test that it returns 'Manager' when the getRole() method is called
-    // define an email for the test
-    // call constructor with an empty string for the name arg, any number for the id arg, and the test email
-    // call the getName() method and check that the result equals the test email
-    it("returns 'Manager' when the getRole() method is called", () => {
-      const manager = new Manager();
+//   // decsribe getRole
+//   describe("getRole", () => {
+//     // test that it returns 'Manager' when the getRole() method is called
+//     // define an email for the test
+//     // call constructor with an empty string for the name arg, any number for the id arg, and the test email
+//     // call the getName() method and check that the result equals the test email
+//     it("returns 'Manager' when the getRole() method is called", () => {
+//       const manager = new Manager();
 
-      expect(manager.getRole()).toBe("Manager");
-    });
-  });
-});
+//       expect(manager.getRole()).toBe("Manager");
+//     });
+//   });
+// });
--- index.js
+++ index.js
@@ -6,54 +6,30 @@ const internQuestions = require("./src/internQuestions.js");
 const htmlTemplate = require("./htmlTemplate");
 const menu = require("./src/menu.js");
 const inquirer = require("inquirer");
-const Employee = require("../lib/Employee");
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Employee = require("./lib/employee");
 
 let teamArray = [];
 // const { hasUncaughtExceptionCaptureCallback } = require("process");
 function init() {
   function createMgr() {
-    inquirer
-      .prompt(managerQuestions)
-      .then((managerQuestionsAnswers) => {
-        let name = managerQuestionsAnswers.mgrName;
-        let idNum = managerQuestionsAnswers.mgrID;
-        let email = managerQuestionsAnswers.mgrEmail;
-        let phone = managerQuestionsAnswers.mgrPhone;
-        let newMgr = new Manager(name, idNum, email, phone);
-      })
-      .then((newMgr) => {
-        teamArray.push(newMgr);
-        console.log(teamArray);
-        menuPrompt();
-      });
+    inquirer.prompt(managerQuestions).then((mgr) => {
+      let manager = new Manager(
+        mgr.mgrName,
+        mgr.mgrID,
+        mgr.mgrEmail,
+        mgr.mgrPhone
+      );
+      teamArray.push(manager);
+      console.log(teamArray);
+      menuPrompt();
+    });
   }
-
-  // inquirer.prompt(questions).then((data) => {
-  //   const filename = `roster.html`;
-  //   fs.writeFile(filename, htmlTemplate.generateMainHtml(data), (err) =>
-  //     err ? console.log(err) : console.log("Success!")
-  //   );
-  //   menuPrompt();
-  // });
-
-  // index.test.js
-  // const questions = require("./questions.js");
-  // jest.mock("inquirer");
-  // jest.mock('fs')
-
-  // describe("init", () => {
-  //   it("should call the prompt method with questions", () => {
-  //     inquirer.prompt.mockReturnValue(
-  //       new Promise( (resolve) => {
-  //         resolve({})
-  //       })
-  //     )
-  //     init();
-
-  //     expect(inquirer.prompt).toBeCalledWith(questions);
-  //     expect(inquirer.prompt()).resolves.toEqual({});
-  //   })
-  // })
 
   function menuPrompt() {
     inquirer.prompt(menu).then((answer) => {
@@ -68,130 +44,33 @@ function init() {
   }
 
   function engineerQuestion() {
-    inquirer
-      .prompt(engineerQuestions)
-      .then((engineerQuestionsAnswers) => {
-        let name = engineerQuestionsAnswers.engName;
-        let idNum = engineerQuestionsAnswers.engID;
-        let email = engineerQuestionsAnswers.engEmail;
-        let github = engineerQuestionsAnswers.engGit;
-        let newEng = new Engineer(name, idNum, email, github);
-      })
-      .then((newEng) => {
-        teamArray.push(newEng);
-        console.log(teamArray);
-        menuPrompt();
-      });
+    inquirer.prompt(engineerQuestions).then((eng) => {
+      let engineer = new Engineer(
+        eng.engName,
+        eng.engID,
+        eng.engEmail,
+        eng.engGit
+      );
+      teamArray.push(engineer);
+      console.log(teamArray);
+      menuPrompt();
+    });
   }
 
   function internQuestion() {
-    inquirer
-      .prompt(internQuestions)
-      .then((internQuestionsAnswers) => {
-        let name = internQuestionsAnswers.intName;
-        let idNum = internQuestionsAnswers.intID;
-        let email = internQuestionsAnswers.intEmail;
-        let school = internQuestionsAnswers.intSchool;
-        let newInt = new Intern(name, idNum, email, school);
-      })
-      .then((newInt) => {
-        teamArray.push(newInt);
-        console.log(teamArray);
-        menuPrompt();
-      });
+    inquirer.prompt(internQuestions).then((int) => {
+      let intern = new Intern(
+        int.intName,
+        int.intID,
+        int.intEmail,
+        int.intSchool
+      );
+      teamArray.push(intern);
+      console.log(teamArray);
+      menuPrompt();
+    });
   }
   createMgr();
-}
-
-class Employee {
-  constructor(name, idNum, email) {
-    this.name = name;
-    this.idNum = idNum;
-    this.email = email;
-  }
-  getName() {
-    return this.name;
-  }
-  getId() {
-    return this.idNum;
-  }
-
-  getEmail() {
-    return this.email;
-  }
-}
-
-class Manager extends Employee {
-  constructor(name, idNum, email, phone) {
-    super(name, idNum, email);
-    this.phone = phone;
-    console.log(this);
-    console.log(`${this.name}created`);
-    console.log(this.name);
-  }
-  getPhone() {
-    return this.phone;
-  }
-
-  getRole() {
-    return "Manager";
-  }
-  // menuPrompt()
-}
-
-class Engineer extends Employee {
-  constructor(name, idNum, email, github) {
-    super(name, idNum, email);
-    this.github = github;
-    console.log(this);
-    console.log(`${this.name}created`);
-    console.log(this.name);
-    // const filename = `${this.name}.html`;
-    // fs.writeFile(filename, generateEngHtml(this), (err) =>
-    //   err ? console.log(err) : console.log("Success!")
-    // );
-    // generateEngineer(this.name);
-    // htmlTemplate.generateEngHtml(
-    //   this.name,
-    //   this.idNum,
-    //   this.email,
-    //   this.github
-    // );
-  }
-  getGithub() {
-    return this.github;
-  }
-  getRole() {
-    return "Engineer";
-  }
-  // menuPrompt();
-}
-
-class Intern extends Employee {
-  constructor(name, idNum, email, school) {
-    super(name, idNum, email);
-    this.school = school;
-    console.log(this);
-    console.log(`${this.name}created`);
-    console.log(this.name);
-    // const filename = `${this.name}.html`;
-    // fs.writeFile(filename, generateIntHtml(this), (err) =>
-    //   err ? console.log(err) : console.log("Success!")
-    // );
-    // htmlTemplate.generateIntHtml(
-    //   this.name,
-    //   this.idNum,
-    //   this.email,
-    //   this.school
-    // );
-  }
-  getSchool() {
-    return this.github;
-  }
-  getRole() {
-    return "Intern";
-  }
-  // menuPrompt();
 }
 
 init();
